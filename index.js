d06999f5f9c0a4ba5199177ad34ef1e8de42d778
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-10-02 23:17:36 -0600

    commit tree 2ee0505dab16409991eda785bc0eaa8c68ecf339

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function  destructivelyAppendCat(Cat1){
+    return cats.push(Cat1)
+}
+function  destructivelyPrependCat(Cat2){
+    return cats.unshift(Cat2)
+}
+
+function  destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function  destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+
+function  appendCat(name){
+    return [...cats,name]
+}
+
+function  prependCat(name){
+    return [name,...cats,]
+}
+
+function  removeLastCat(){
+    return [cats[0],cats[1]]
+}
+
+function  removeFirstCat(){
+    return [cats[1],cats[2]]
+}
